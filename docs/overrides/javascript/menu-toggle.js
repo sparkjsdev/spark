5aac4f44492e0ef030af0ddef0b3fef6ecbd9522
Joe Strouth <joestrouth1@users.noreply.github.com>
2025-07-08 21:51:41 -0400

    chore: cleanup

--- docs/overrides/javascript/menu-toggle.js
+++ docs/overrides/javascript/menu-toggle.js
@@ -2,7 +2,6 @@ const menuTrigger = document.getElementById("menu-toggle");
 const drawerToggle = document.querySelector(
   'input[type=checkbox][data-md-toggle="drawer"]'
 );
-console.info({ menuTrigger, drawerToggle });
 if (menuTrigger && drawerToggle) {
   menuTrigger.addEventListener("click", () => {
     drawerToggle.checked = !drawerToggle.checked;
