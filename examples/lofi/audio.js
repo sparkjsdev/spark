6cc89f1630a7bce75720db199ea82c4254d81ee5
Andreas Sundquist <asundqui@worldlabs.ai>
2025-12-02 10:58:32 -0800

    SparkXr hand tracking. Added examples/lofi as Lofi Worlds Marble implemented in Spark.

---
+++ examples/lofi/audio.js
@@ -0,0 +1,13 @@
+export function getAudioUrl(file) {
+  // return `./lofi/${file}`;
+  return `https://wlt-ai-cdn.art/lofi/${file}`;
+}
+
+export const AUDIOS = [
+  "lofi-1.mp3",
+  "lofi-2.mp3",
+  "lofi-3.mp3",
+  "lofi-4.mp3",
+  "lofi-5.mp3",
+  "lofi-6.mp3",
+];
