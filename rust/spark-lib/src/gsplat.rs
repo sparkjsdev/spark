32efc2406428dcd52a29d6d3f2a0e901d8b81cfd
Andreas Sundquist <asundqui@worldlabs.ai>
2026-06-01 15:47:37 -0700

    Rename TsplatArray::set_max_sh_degree to better clamp_sh_degree, fix logic. Update build-lod to include clamping and final max_sh in RAD comments.

--- rust/spark-lib/src/gsplat.rs
+++ rust/spark-lib/src/gsplat.rs
@@ -253,9 +253,9 @@ impl TsplatArray for GsplatArray {
         self.max_sh_degree
     }
 
-    fn set_max_sh_degree(&mut self, max_sh_degree: usize) {
+    fn clamp_sh_degree(&mut self, max_sh_degree: usize) {
         assert!(max_sh_degree <= 3, "SH degrees must be between 0 and 3");
-        self.max_sh_degree = max_sh_degree;
+        let max_sh_degree = max_sh_degree.min(self.max_sh_degree);
 
         if max_sh_degree < 3 {
             self.sh3.clear();
@@ -266,6 +266,7 @@ impl TsplatArray for GsplatArray {
         if max_sh_degree < 1 {
             self.sh1.clear();
         }
+        self.max_sh_degree = max_sh_degree;
     }
 
     fn len(&self) -> usize {
