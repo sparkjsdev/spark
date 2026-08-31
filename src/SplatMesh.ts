12d88e3fd8260d3d0b5857aec313fca50c109084
Noeri Huisman <8823461+mrxz@users.noreply.github.com>
2026-08-31 15:29:13 +0200

    Make `SplatEdit` and `SplatEditSdf` tree-shakable

--- src/SplatMesh.ts
+++ src/SplatMesh.ts
@@ -9,7 +9,13 @@ import {
 import { ExtSplats } from "./ExtSplats";
 import { PackedSplats } from "./PackedSplats";
 import { type RgbaArray, TRgbaArray } from "./RgbaArray";
-import { SplatEdit, SplatEditSdf, SplatEdits } from "./SplatEdit";
+import {
+  type SplatEdit,
+  type SplatEditSdf,
+  SplatEdits,
+  isSplatEdit,
+  isSplatEditSdf,
+} from "./SplatEdit";
 import {
   type CovSplatModifier,
   CovSplatTransformer,
@@ -949,7 +955,7 @@ export class SplatMesh extends SplatGenerator {
     if (this.editable && !this.edits) {
       // If we haven't set any explicit edits, add any child SplatEdits
       this.traverseVisible((node) => {
-        if (node instanceof SplatEdit) {
+        if (isSplatEdit(node)) {
           edits.push(node);
         }
       });
@@ -962,7 +968,7 @@ export class SplatMesh extends SplatGenerator {
       }
       const sdfs: SplatEditSdf[] = [];
       edit.traverseVisible((node) => {
-        if (node instanceof SplatEditSdf) {
+        if (isSplatEditSdf(node)) {
           sdfs.push(node);
         }
       });
